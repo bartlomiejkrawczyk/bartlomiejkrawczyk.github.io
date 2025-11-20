3e14a8159b65265e523793c315abe9559af8f327
Bartłomiej Krawczyk <bartikraw@gmail.com>
2025-11-20 20:56:59 +0100

    Configure basic command line

--- src/components/popups/Commands.ts
+++ src/components/popups/Commands.ts
@@ -1,4 +1,11 @@
-export const commandIds = ["go-to-home", "open-github"] as const;
+export const commandIds = [
+  "print",
+  "go-to-home",
+  "go-back",
+  "open-github",
+  "open-instagram",
+  "open-discord",
+] as const;
 
 export type CommandId = (typeof commandIds)[number];
 
@@ -9,18 +16,46 @@ export interface Command {
 }
 
 export const commands: Record<CommandId, Command> = {
+  print: {
+    id: "print",
+    name: { en: "Print", pl: "Drukuj" },
+    execute: () => {
+      window.print();
+    },
+  },
   "go-to-home": {
     id: "go-to-home",
-    name: { en: "Go to home", pl: "Przejdź do Strony Głównej" },
+    name: { en: "Home", pl: "Strona Główna" },
     execute: () => {
       window.location.href = "/";
     },
   },
+  "go-back": {
+    id: "go-back",
+    name: { en: "Back", pl: "Poprzednia Strona" },
+    execute: () => {
+      history.back();
+    },
+  },
   "open-github": {
     id: "open-github",
-    name: { en: "Open GitHub", pl: "Otwórz GitHub" },
+    name: { en: "GitHub", pl: "GitHub" },
     execute: () => {
       window.open("https://github.com/bartlomiejkrawczyk");
+    },
+  },
+  "open-instagram": {
+    id: "open-instagram",
+    name: { en: "Instagram", pl: "Instagram" },
+    execute: () => {
+      window.open("https://www.instagram.com/bartlomiejkrawczyk_");
+    },
+  },
+  "open-discord": {
+    id: "open-discord",
+    name: { en: "Discord", pl: "Discord" },
+    execute: () => {
+      window.open("https://discordapp.com/users/384450810547011584");
     },
   },
 };
