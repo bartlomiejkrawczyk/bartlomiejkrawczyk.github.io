28a42daac0b39ec9294b867f15a00db37181c030
Bartłomiej Krawczyk <bartikraw@gmail.com>
2026-06-11 20:53:24 +0200

    Refactor SelectionGrid to utilize previousAndNext function for navigation, improving code clarity and maintainability

--- src/components/date/items.ts
+++ src/components/date/items.ts
@@ -14,3 +14,20 @@ export const dateSteps = [
 export type DateStep = (typeof dateSteps)[number];
 
 export const DATE_STEPS = dateSteps.length;
+
+export function previousAndNext(step: DateStep) {
+  let next: string | undefined;
+  let back: string | undefined;
+
+  for (let i = 0; i < DATE_STEPS; i++) {
+    if (dateSteps[i] !== step) continue;
+    next = dateSteps[i + 1];
+    if (i == 1) continue;
+    back = dateSteps[i - 1];
+  }
+
+  return {
+    back: back,
+    next: next,
+  };
+}
